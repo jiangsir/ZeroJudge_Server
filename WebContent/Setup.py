c6969ff7c109049101e7559be9f4791d6ddd01b4
jiangsir <jiangzero@gmail.com>
2016-09-06 13:45:48 +0800

    把 CONSOLE 移到 build.xml 處理。

--- WebContent/Setup.py
+++ WebContent/Setup.py
@@ -6,30 +6,22 @@ import os
 # echo $path
 path = os.path.dirname(os.path.realpath(__file__))
 
-print("path="+path)
+for file in os.listdir('/etc/init.d/'):
+    if fnmatch.fnmatch(file, 'tomcat*'):
+        tomcatN = file
+
+
+print("path=" + path)
 os.system('sudo apt-get install sudo')
 os.system('sudo apt-get install ntpdate')
 os.system('sudo apt-get install dos2unix')
 os.system('sudo apt-get install rsync')
 os.system('sudo apt-get install default-jdk')
-os.system('sudo apt-get install g++')
 os.system('sudo apt-get install fpc')
+os.system('sudo apt-get install g++')
 
-os.system('sudo find ' + path + ' -name "*.sh" -exec chmod +x {} +')
-os.system('sudo find ' + path + ' -name "*.exe" -exec chmod +x {} +')
-os.system('sudo find ' + path + ' -name "*.class" -exec chmod +x {} +')
-os.system('sudo chmod a+w ' + path + '/JudgeServer_CONSOLE/Testdata')
-os.system('sudo chmod a+w ' + path + '/JudgeServer_CONSOLE/Special')
-os.system('sudo chmod a+w ' + path + '/JudgeServer_CONSOLE/Executable')
+os.system('sudo cat /dev/null > /var/log/' + tomcatN + '/catalina.out')
+# os.system('sudo find /etc/init.d/ -name "tomcat*" -exec bash -c "{} restart" \;')
+os.system('sudo /etc/init.d/' + tomcatN + ' restart')
 
-os.system('sudo rm -rf /JudgeServer_CONSOLE')
-os.system('sudo mv ' + path + '/JudgeServer_CONSOLE /')
-
-os.system('sudo g++ -o /JudgeServer_CONSOLE/Bin/shell.exe /JudgeServer_CONSOLE/Bin/shell.cpp')
-os.system('sudo gcc /JudgeServer_CONSOLE/Bin/base_c.c -o /JudgeServer_CONSOLE/Bin/base_c.exe')
-os.system('sudo g++ -o /JudgeServer_CONSOLE/Bin/base_cpp.exe /JudgeServer_CONSOLE/Bin/base_cpp.cpp')
-
-
-os.system('sudo cat /dev/null > /var/log/tomcat8/catalina.out')
-os.system('sudo find /etc/init.d/ -name "tomcat*" -exec bash -c "{} restart" \;')
 os.system('sudo rm ' + os.path.realpath(__file__))
